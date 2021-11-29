8cdb2601816298a11083934451787744478d7eb5
kokoaono <nyswzt.a@gmail.com>
2021-11-29 14:01:28 +1300

    fail to populate edit changes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,22 +6,22 @@ import { removeExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
   return (
-   <div>
-    <ExpenseForm
-      expense={props.expense}
-      onSubmit={(expense) => {
-        props.dispatch(editExpense(props.expense.id, expense));
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button onClick={() => {
+        props.dispatch(removeExpense({ id: props.expense.id }));
         props.history.push('/');
-    }}
-    />
-    <button onClick={() => {
-      props.dispatch(removeExpense({ id: props.expense.id }));
-      props.history.push('/');
-    }}
-    >
-      Remove
-    </button>
-  </div>
+      }}
+      >
+        Remove
+      </button>
+    </div>
   );
 };
 
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,19 +4,19 @@ import { SingleDatePicker } from 'react-dates';
 // import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
-  constructor(props)  {
+  constructor(props) {
     super(props);
 
-    this.state={
-    description: props.expense ? props.expense.description : '',
-    note: props.expense ? props.expense.note : '',
-    amount: props.expense ? (props.expense.amount / 100).toString() : '',
-    createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
-    calendarFocused: false,
-    error: ''
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: ''
     }
-  }
- 
+  };
+
   onDescriptionChange = (e) => {
     const description = e.target.value
     this.setState(() => ({ description }));
@@ -35,19 +35,19 @@ export default class ExpenseForm extends React.Component {
   };
 
   onDateChange = (createdAt) => {
-    if(createdAt) {
+    if (createdAt) {
       this.setState(() => ({ createdAt }))
     }
   };
 
-  onFocusChange = ({focused }) => {
-    this.setState(() => ({calendarFocused: focused}))
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }))
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    
-    if(!this.state.description || !this.state.amount) {
+
+    if (!this.state.description || !this.state.amount) {
       this.setState(() => ({ error: 'Please provide description and amount' }))
     } else {
       this.setState(() => ({ error: '' }));
@@ -60,7 +60,7 @@ export default class ExpenseForm extends React.Component {
     }
   };
 
-  render () {
+  render() {
     return (
       <div>
         {this.state.error && <p>{this.state.error}</p>}
@@ -72,7 +72,7 @@ export default class ExpenseForm extends React.Component {
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
-          <input 
+          <input
             type="text"
             placeholder="Amount"
             value={this.state.amount}
@@ -85,16 +85,16 @@ export default class ExpenseForm extends React.Component {
             onFocusChange={this.onFocusChange}
             numberOfMonths={1}
             isOutsideRange={() => false}
-          /> 
+          />
           <textarea
-          placeholder="Add a note for your expense (optional)"
-          value={this.state.note}
-          onChange={this.onNoteChange}
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
           >
           </textarea>
           <button>Add Expense</button>
         </form>
       </div>
     )
-  }  
+  }
 }
